dd41dfcfb05f1c596abe2140df27f34be2b3c0a7
ishu1820 <ishitamahajan9449@gmail.com>
2025-06-29 13:35:06 +0530

    logging framework added

---
+++ week 2/loggingframework/src/main/java/com/example/Login.java
@@ -0,0 +1,12 @@
+package com.example;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public class Login 
+{
+    private static final Logger logger = LoggerFactory.getLogger(Login.class);
+    public static void main( String[] args )
+    {
+        logger.error("this is an error message");
+        logger.warn("this is a warning message");
+}
+}
