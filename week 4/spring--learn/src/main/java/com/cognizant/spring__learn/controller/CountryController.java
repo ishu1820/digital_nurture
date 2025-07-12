71790b8f19fec0a354bf6585cd230b70cd832971
ishu1820 <ishitamahajan9449@gmail.com>
2025-07-12 11:10:34 +0530

    restful country web service

---
+++ week 4/spring--learn/src/main/java/com/cognizant/spring__learn/controller/CountryController.java
@@ -0,0 +1,20 @@
+package com.cognizant.spring__learn.controller;
+import  com.cognizant.spring__learn.model.Country;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+@RestController
+public class CountryController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CountryController.class);
+    @RequestMapping("/country")
+    public Country getCountryIndia(){
+        LOGGER.info("START getCountryIndia()");
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Country country = (Country)context.getBean("in");
+        LOGGER.info("END getCountryIndia()");
+        return country;
+    }
+}
